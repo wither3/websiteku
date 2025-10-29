f45465fd600c3e738d4b978827ff71893b05f04c
wither3 <84785651+wither3@users.noreply.github.com>
2025-10-29 20:30:41 +0800

    Add files via upload

--- java.js
+++ java.js
@@ -13,3 +13,55 @@ document.getElementById('menu-button').addEventListener('click', function() {
       document.getElementById('overlay').classList.add('hidden');
     });
 
+document.getElementById('tiktokBtn').addEventListener('click', function() {
+  window.location.href = 'https://www.tiktok.com/@qhairulpratama?_t=ZS-90x9lWBVmYW&_r=1';
+});
+
+document.getElementById('YTBtn').addEventListener('click', function() {
+  window.location.href = 'https://youtube.com/@craft7yt332?si=AVXAWvopU-UJeDqs';
+});
+
+document.getElementById('IgBtn').addEventListener('click', function() {
+  window.location.href = 'https://www.instagram.com/rullznpc?igsh=MWVlZG9tMHA3dWQ4eg==';
+});
+
+
+
+
+
+
+const audio = document.getElementById("audio");
+    const playBtn = document.getElementById("playBtn");
+    const bar = document.getElementById("bar");
+    const progress = document.getElementById("progress");
+
+    // play / pause toggle
+    playBtn.addEventListener("click", () => {
+      if (audio.paused) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
+    });
+
+    // ubah ikon tombol
+    audio.addEventListener("play", () => {
+      playBtn.innerHTML = '<i class="fa fa-pause"></i>';
+    });
+    audio.addEventListener("pause", () => {
+      playBtn.innerHTML = '<i class="fa fa-play"></i>';
+    });
+
+    // update progress bar
+    audio.addEventListener("timeupdate", () => {
+      const percent = (audio.currentTime / audio.duration) * 100;
+      bar.style.width = percent + "%";
+    });
+
+    // klik progress untuk seek
+    progress.addEventListener("click", (e) => {
+      const rect = progress.getBoundingClientRect();
+      const offsetX = e.clientX - rect.left;
+      const percent = offsetX / rect.width;
+      audio.currentTime = percent * audio.duration;
+    });
