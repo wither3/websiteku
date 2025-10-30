54fc3bd34560c61354f798f58956fbc5fce8b2d4
wither3 <84785651+wither3@users.noreply.github.com>
2025-10-30 17:21:52 +0800

    Add files via upload

---
+++ downloadPeta.js
@@ -0,0 +1,39 @@
+document.getElementById('menu-button').addEventListener('click', function() {
+      document.getElementById('sidebar').classList.toggle('open');
+      document.getElementById('overlay').classList.toggle('hidden');
+    });
+
+    document.getElementById('overlay').addEventListener('click', function() {
+      document.getElementById('sidebar').classList.remove('open');
+      document.getElementById('overlay').classList.add('hidden');
+    });
+
+    document.getElementById('close-button').addEventListener('click', function() {
+      document.getElementById('sidebar').classList.remove('open');
+      document.getElementById('overlay').classList.add('hidden');
+    });
+
+document.getElementById('whatsapp').addEventListener('click', function() {
+  window.location.href = 'https://chat.whatsapp.com/FZtfxpSyYCN9FyCjWekyZN?mode=wwt';
+});
+
+document.getElementById('downloadPetaBedrock').addEventListener('click', function() {
+  window.location.href = 'https://sfile.mobi/1VKukAw2by6';
+});
+
+document.getElementById('downloadPetaJava').addEventListener('click', function() {
+  window.location.href = 'https://sfile.mobi/1zAu8HNRZuw';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
