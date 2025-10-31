750dd64ed81518102ce9bc779e89babd3d3b833c
wither3 <84785651+wither3@users.noreply.github.com>
2025-10-31 13:25:41 +0800

    Remove TikTok stalking endpoint
    Removed TikTok user stalking endpoint from the server.

--- index.js
+++ index.js
@@ -71,21 +71,7 @@ app.get("/ttsave", async (req, res) => {
 });
 
 
-app.get('/tikstalk', async (req, res) => {
-    const username = req.query.username; // Ambil username dari query parameter
 
-    if (!username) {
-        return res.status(400).json({ error: 'Username TikTok diperlukan!' });
-    }
-
-    try {
-        const data = await tiktokStalk(username); // Panggil fungsi stalk
-        res.status(200).json({ success: true, data }); // Kirim data sebagai respons
-    } catch (error) {
-        console.error('Gagal mendapatkan data TikTok:', error);
-        res.status(500).json({ success: false, error: 'Terjadi kesalahan pada server!' });
-    }
-});
 
 
 
