50bccb3a3baa06c9c310d747c2cbc4bf4c6baf24
wither3 <84785651+wither3@users.noreply.github.com>
2025-11-04 17:17:29 +0800

    Modify API usage message in index.js
    Updated usage message to reflect ongoing development.

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.get('/bagian1/:api?', async (req, res) => {
     return res.json({
       status: true,
       message: "API EnderNet aktif!",
-      usage: "/bagian1/datanyaaa atau ?api=datanyaaa"
+      usage: "Maaf ini masih di kembangkan oleh rullzNPC bersama ChatGPT, Kenapa harus chatGPT karena aku tidak punya teman yang jago coding."
     });
   }
 
